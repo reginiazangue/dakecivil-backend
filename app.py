74eeb96e5aa82a82fe17b0f9a55cb1919fe3fe32
KOUODZO ZANGUE REGINIA <reginiazangue84@gmail.com>
2025-12-27 09:58:47 +0100

    Mise à jour frontend React et backend Flask

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def contact():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-   
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)   
